207c3c0598d4c4fb851a4478549ccdeb0901d244
byeongjokim <bjbj8083@gmail.com>
2019-01-04 12:56:49 +0900

    [dev -ed] prototype

--- web/home.py
+++ web/home.py
@@ -4,3 +4,7 @@ home = Blueprint('home', __name__, template_folder='templates')
 @home.route("/main")
 def main():
     return render_template("main.html")
+
+@home.route("/intro")
+def intro():
+    return render_template("intro.html")
