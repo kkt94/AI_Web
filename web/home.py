97f8c8b97f10ef339d6a906798a2acc48e1ebe0e
Yuri-Kim <yuri.kim.dev@gmail.com>
2019-05-01 13:19:28 +0900

    [dev -ing] chatbot

--- web/home.py
+++ web/home.py
@@ -1,6 +1,16 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
+from optparse import OptionParser
+import socket, random, sys
 
 home = Blueprint('home', __name__, template_folder='templates')
+
+# ChatScript
+cs_host = "166.104.143.103"
+cs_port = 1024
+cs_addr = (cs_host, cs_port)
+
+cs_bot = 'ailabfive'
+cs_master = 'user'
 
 @home.route("/")
 @home.route("/main")
@@ -51,3 +61,36 @@ def link():
         links.append({"type": t, "links": l})
 
     return render_template("link.html", links=links)
+
+@home.route("/chatbot")
+def chatbot():
+    return render_template("chatbot.html")
+
+@home.route("/response", methods=['POST'])
+def response():
+    result = request.get_json()
+    username = request.remote_addr + request.headers.get('User-Agent')
+    message = username + chr(0) + cs_bot + chr(0) + result + chr(0)
+
+    if len(result):
+        split_result = result.split(' ')[0]
+        if split_result == ":reset" or split_result == ":pos" or split_result == ":why" or split_result == ":build":
+            message = cs_master + chr(0) + cs_bot + chr(0) + result + chr(0)
+        message = message.encode("utf-8") # original
+        # message = base64.b64encode(message)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.bind(("166.104.143.103", random.randrange(1025)+1024))
+        client_socket.connect(cs_addr)
+        client_socket.send(message)
+        data_out = ''
+        while True:
+            buf = client_socket.recv(64)
+            if len(buf) > 0:
+                # data_out += buf.decode("utf-8")
+                data_out += buf.decode("utf-8", errors='ignore') # original
+                # data_out += buf.decode("utf-16")
+            else:
+                break
+        client_socket.close()
+
+        return jsonify(data_out)
---
+++ web/static/js/app.js
@@ -0,0 +1,118 @@
+(function () {
+  var Message;
+
+  Message = function (arg) {
+    console.log('Messaage = function')
+    this.text = arg.text, this.message_side = arg.message_side;
+    this.draw = function (_this, timer) {
+      return function (timer) {
+        var $message;
+        if(_this.message_side=='right'){
+          $message = $($('.message_template_right').clone().html());
+        }
+        else{
+          $message = $($('.message_template_left').clone().html());
+        }
+        setTimeout(function () {
+          $message.addClass(_this.message_side).find('.text').html("...");
+          $('.messages').append($message);
+          return $message.addClass('appeared');
+        }, timer - (_this.text.split(' ').length)*350);
+        setTimeout(function () {
+          return $('.messages').animate({ scrollTop: $('.messages').prop('scrollHeight') }, 300);
+        }, timer - (_this.text.split(' ').length)*350);
+        setTimeout(function () {
+          return $message.addClass(_this.message_side).find('.text').html(_this.text);
+        }, timer);
+        return setTimeout(function () {
+          return $('.messages').animate({ scrollTop: $('.messages').prop('scrollHeight') }, 300);
+        }, timer);
+      };
+    }(this);
+    return this;
+  };
+
+  $(function () {
+    var getMessageText, printMessage;
+    getMessageText = function () {
+      var $message_input;
+      $message_input = $('.message_input');
+      return $message_input.val();
+    };
+    printMessage = function (text, message_side, timer) {
+      console.log('printMessage = function')
+      var $messages, message;
+      if (text.trim() === '') {
+        return;
+      }
+      $('.message_input').val('');
+      $messages = $('.messages');
+      message = new Message({
+        text: text,
+        message_side: message_side
+      });
+      message.draw(timer);
+      setTimeout(function () {
+      }, timer);
+      return 1;
+    };
+
+    sendMessage = function (text) {
+      console.log('sendMessaage = function')
+      response_text = ''
+      $.ajax({
+        url:"response",
+        type:"POST",
+        data:JSON.stringify(text),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(data){
+          timer = 0;
+          data.split('\\n').forEach(function (item, index) {
+            timer += (item.split(' ').length)*350;
+            printMessage(item, 'left', timer);
+          });
+        }
+      });
+      return response_text;
+    };
+
+    sendMessageNoDelay = function (text) {
+      console.log('sendMessaageNoDelay = function')
+      response_text = ''
+      $.ajax({
+        url:"response",
+        type:"POST",
+        data:JSON.stringify(text),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(data){
+          timer = 0;
+          data.split('\\n').forEach(function (item, index) {
+            printMessage(item, 'left', timer);
+            timer += (item.split(' ').length)*100;
+          });
+        }
+      });
+      return response_text;
+    };
+
+    $('.send_message').click(function (e) {
+      console.log('.send_message.click function')
+      input_text = getMessageText();
+      printMessage(input_text, 'right');  //print input
+      sendMessage(input_text); //print response
+    });
+
+    $('.message_input').keyup(function (e) {
+      console.log('.message_input.keyup function')
+      if (e.which === 13) {
+        input_text = getMessageText();
+        printMessage(input_text, 'right');  //print input
+        sendMessage(input_text); //print response
+      }
+    });
+    sendMessageNoDelay(''); // original
+    // sendMessageNoDelay(input_text);
+  });
+}.call(this));
