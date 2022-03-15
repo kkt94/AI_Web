e1f475cd5372cc07ac53710f7504ea6bc8ccfe3a
ToBeSuperior <ksh970404@naver.com>
2022-03-15 21:17:05 +0900

    test

--- web/static/js/ai_web.js
+++ web/static/js/ai_web.js
@@ -1,10 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -23,4 +25,3 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
